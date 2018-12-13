df070d492748fcb68413172dcff939f277229552
Yarn <ztytotoro@users.noreply.github.com>
2018-12-13 21:01:10 +0800

    Update check-repeat-files.py

--- check-repeat-files.py
+++ check-repeat-files.py
@@ -1,14 +1,14 @@
 import hashlib
 import os
 import sys
+import time
 
 
 def check_files(path):
+    start_time = time.time()
     file_dict = {}
     files = get_all_files(path)
     progress = 0
-    sys.stdout.write("\n")
-    sys.stdout.flush()
     for fileName in files:
         md5 = hashlib.md5()
         f = open(fileName, "rb")
@@ -21,25 +21,28 @@ def check_files(path):
         else:
             file_dict[md5.hexdigest()].append(fileName)
         progress += 1
-        sys.stdout.write("\r已扫描 [%d]" % (progress / len(files) * 100))
+        sys.stdout.write("\r已扫描 [%d%%]" % (progress / len(files) * 100))
         sys.stdout.flush()
     sys.stdout.write("\r                ")
     sys.stdout.flush()
     count = 0
     for (key, value) in filter(lambda x: len(x[1]) > 1, file_dict.items()):
-        print("重复[%d]：" % len(value))
+        print("\n重复[%d]：" % len(value))
         count += len(value)
         for file in value:
             print("     %s" % file)
-    print("\n扫描完成！共发现%d个重复文件" % count)
+    end_time = time.time()
+    print("\n扫描完成！共发现%d个重复文件, 耗时%d秒\n" % (count, end_time - start_time))
+
 
 def get_all_files(path):
-    files = []
+    result = []
     if os.path.exists(path):
         for (root, dirs, files) in os.walk(path):
             for file in files:
-                files.append(os.path.join(root, file))
-    return files
+                result.append(os.path.join(root, file))
+    return result
+
 
 def main(argv=None):
     if argv is None:
