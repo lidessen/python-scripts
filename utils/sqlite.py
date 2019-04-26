a8fa13856d77ecd6932d4849aff0d965b0173022
Yarn <ztytotoro@users.noreply.github.com>
2019-04-26 17:56:44 +0800

    update

--- utils/sqlite.py
+++ utils/sqlite.py
@@ -1,1 +1,24 @@
-import sqlite3
+# 导入:
+from sqlalchemy import Column, String, create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+# 创建对象的基类:
+Base = declarative_base()
+
+
+# 定义Project对象:
+class Project(Base):
+    # 表的名字:
+    __tablename__ = 'project'
+
+    # 表的结构:
+    id = Column(String(20), primary_key=True)
+    name = Column(String(20))
+
+
+# 初始化数据库连接:
+engine = create_engine(
+    'mysql+mysqlconnector://root:password@localhost:3306/test')
+# 创建DBSession类型:
+DBSession = sessionmaker(bind=engine)
